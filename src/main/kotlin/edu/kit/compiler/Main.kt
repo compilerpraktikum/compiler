94d961b38c918b8327ec0e76710b4cbf3c8957f4
Tobias Stickling <uudyh@student.kit.edu>
2021-10-22 15:22:22 +0200

    Restructure argument parsing to reduce duplicated code.

--- src/main/kotlin/edu/kit/compiler/Main.kt
+++ src/main/kotlin/edu/kit/compiler/Main.kt
@@ -5,22 +5,16 @@ import kotlinx.cli.ArgType
 import kotlinx.cli.default
 import java.io.File
 
-data class Config(
-    val isEcho: Boolean
-)
-
 fun main(args: Array<String>) {
     val parser = ArgParser("mjavac")
 
-    val isEchoMode by parser.option(ArgType.Boolean, fullName = "echo", description = "Write input file to stdout").default(false)
+    val config = object : Compiler.Config {
+        override val isEcho by parser.option(ArgType.Boolean, fullName = "echo", description = "Write input file to stdout").default(false)
+    }
 
     val file by parser.argument(ArgType.String, fullName = "file", description = "input file")
 
     parser.parse(args)
-
-    val config = Config(
-        isEcho = isEchoMode
-    )
 
     Compiler(config).compile(File(file))
 }
