e54c1af1a1eab63206d858f991de4e6108b9f43a
Tobias Stickling <uudyh@student.kit.edu>
2021-10-22 13:12:32 +0200

    Add argument parsing and echo mode.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,24 @@
-fun main() {
-    println("Hello World")
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import kotlinx.cli.default
+import java.io.File
+
+data class Config(
+    val isEcho: Boolean
+)
+
+fun main(args: Array<String>) {
+    val parser = ArgParser("mjavac")
+
+    val isEchoMode by parser.option(ArgType.Boolean, fullName = "echo", description = "Write input file to stdout").default(false)
+
+    val file by parser.argument(ArgType.String, fullName = "file", description = "input file")
+
+    parser.parse(args)
+
+    val config = Config(
+        isEcho = isEchoMode
+    )
+
+    Compiler(config).compile(File(file))
 }
