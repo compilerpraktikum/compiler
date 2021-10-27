60315ff13ea845d10e70791ce0d989bcc35f6e76
Tobias Stickling <uudyh@student.kit.edu>
2021-10-27 14:13:00 +0200

    Implement string table.

--- src/main/kotlin/edu/kit/compiler/lex/StringTable.kt
+++ src/main/kotlin/edu/kit/compiler/lex/StringTable.kt
@@ -1,10 +1,49 @@
 package edu.kit.compiler.lex
 
+import java.util.*
+
 /**
- * Wrapping facade to a string table that is generated during lexicographic analysis and later utilized during
+ * Stores a mapping between identifier names and associated information ([StringTable.Entry]). It also handles internalizing
+ * identifier names to save time and memory.
+ * The string table is generated during lexicographic analysis and later utilized during
  * parsing and during semantic analysis.
  */
 class StringTable {
-
-    // ... implementation using hash table
+    
+    private val table = IdentityHashMap<String, Entry>()
+    
+    /**
+     * Tries to register a new identifier if not already known.
+     *  @return the internalized identifier name
+     */
+    fun tryRegisterIdentifier(name: String): String {
+        val internName = name.intern()
+        table.putIfAbsent(internName, Entry(isKeyword = false))
+        return internName
+    }
+    
+    /**
+     * Registers a new keyword.
+     * @throws[IllegalArgumentException] if the keyword is already registered
+     */
+    fun registerKeyword(name: String) {
+        val internName = name.intern()
+        require(!table.contains(internName)) { "keyword already registered" }
+        table[internName] = Entry(isKeyword = true)
+    }
+    
+    /**
+     * Get the [Entry] associated with the given identifier name.
+     */
+    fun getEntryOrNull(name: String): Entry? {
+        return table[name.intern()]
+    }
+    
+    /**
+     * Stores information (-> TODO) associated to an identifier.
+     */
+    data class Entry(
+        val isKeyword: Boolean
+    )
+    
 }
