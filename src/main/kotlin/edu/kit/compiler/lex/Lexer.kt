f4f3737fea460653cc8de046c28a8c0ca1a2dd79
Johannes Hengstler <ubezl@student.kit.edu>
2021-10-27 18:00:27 +0200

    introduced `InputProvider` interface for RingBuffer, so test cases can exchange the buffer without mocking

--- src/main/kotlin/edu/kit/compiler/lex/Lexer.kt
+++ src/main/kotlin/edu/kit/compiler/lex/Lexer.kt
@@ -10,8 +10,7 @@ import kotlinx.coroutines.flow.flow
  * @param input input abstracted in a ring buffer
  * @param stringTable string table of current compilation run
  */
-class Lexer(private val input: RingBuffer, private val stringTable: StringTable
-) {
+class Lexer(private val input: InputProvider, private val stringTable: StringTable) {
     
     fun tokenStream() = flow {
         val c = input.nextChar()
--- src/main/kotlin/edu/kit/compiler/lex/RingBuffer.kt
+++ src/main/kotlin/edu/kit/compiler/lex/RingBuffer.kt
@@ -11,68 +11,67 @@ import java.nio.channels.ScatteringByteChannel
  * A ring-buffered interface to read the compilation unit. It asynchronously reads into two [ByteBuffers][ByteBuffer]
  * and swaps between them when reading characters.
  */
-class RingBuffer(private val inputChannel: ScatteringByteChannel) {
-
+class RingBuffer(private val inputChannel: ScatteringByteChannel) : InputProvider {
+    
     private companion object {
         const val BUFFER_SIZE = 128
     }
-
+    
     /**
      * A ring buffer with one buffer to read from, one to peek into and one to load in the background.
      */
     private val ring = arrayOf(LoadableBuffer(), LoadableBuffer(), LoadableBuffer())
-
+    
     /**
      * Index of the currently active block
      */
     @Volatile
     private var currentBlockIndex: Int = 0
-
+    
     /**
      * Whether the end of the input channel has been reached
      */
     @Volatile
     private var endOfInput: Boolean = false
-
+    
     /**
      * how many blocks have been read in total
      */
     @Volatile
     private var totalLoadedBlocks = 0
-
+    
     init {
         ring.forEach { runBlocking { it.load() } }
     }
-
+    
     /**
      * Obtain the next character and advance the current buffer position by one
      */
-    suspend fun nextChar(): Char? = coroutineScope {
+    override suspend fun nextChar(): Char? = coroutineScope {
         // if we already exceed the maximum amount of possible blocks, return null
-        if (currentBlockIndex >= totalLoadedBlocks && endOfInput)
-            return@coroutineScope null
-
+        if (currentBlockIndex >= totalLoadedBlocks && endOfInput) return@coroutineScope null
+        
         val currentBuffer = ring[currentBlockIndex % ring.size]
-
+        
         currentBuffer.waitUntilReady()
-
+        
         // throw exception to caller, if previous load failed
         if (currentBuffer.failed) {
             throw currentBuffer.failure!!
         }
-
+        
         val remainingInCurrentBuffer = currentBuffer.byteBuffer.remaining()
-
+        
         // check if current buffer is empty. If it is, we must have reached end of input,
         // because otherwise we would have switched to the next buffer
         if (remainingInCurrentBuffer == 0) {
             assert(endOfInput)
             return@coroutineScope null
         }
-
+        
         // get current byte
         val currentByte = currentBuffer.byteBuffer.get()
-
+        
         // if the buffer is now exhausted, and we have more data, schedule a new read and swap buffer
         if (remainingInCurrentBuffer == 1) {
             if (!endOfInput) {
@@ -80,60 +79,58 @@ class RingBuffer(private val inputChannel: ScatteringByteChannel) {
                     currentBuffer.load()
                 }
             }
-
+    
             // swap to next buffer
             currentBlockIndex += 1
         }
-
+        
         return@coroutineScope Char(currentByte.toUByte().toInt())
     }
-
+    
     /**
      * Lookahead in the buffer by a given [offset]. By default, peeks ahead 0 characters and thus returns the
      * character that would be returned by [nextChar].
      * Returns `null` if no character is available in the input.
      */
-    suspend fun peek(offset: Int = 0): Char? {
+    override suspend fun peek(offset: Int): Char? {
         require(offset < BUFFER_SIZE) { "look-ahead cannot exceed buffer size of $BUFFER_SIZE bytes" }
         return peekInBlock(currentBlockIndex, offset)
     }
-
+    
     /**
      * Recursive function that peeks into buffers once they are loaded, or calls the function again to load the next
      * buffer
      */
     private tailrec suspend fun peekInBlock(blockIndex: Int, offset: Int): Char? {
         // if we exceed the number of loaded blocks and have reached EOF yet, we can return null
-        if (blockIndex >= totalLoadedBlocks && endOfInput)
-            return null
-
+        if (blockIndex >= totalLoadedBlocks && endOfInput) return null
+        
         val currentBuffer = ring[blockIndex % ring.size]
         currentBuffer.waitUntilReady()
-
+        
         // check how many bytes are in current buffer
         val remainingInCurrentBuffer = currentBuffer.byteBuffer.remaining()
         return if (offset < remainingInCurrentBuffer) {
-            val peekedByte = currentBuffer.byteBuffer
-                .get(currentBuffer.byteBuffer.position() + offset)
-
+            val peekedByte = currentBuffer.byteBuffer.get(currentBuffer.byteBuffer.position() + offset)
+            
             Char(peekedByte.toUByte().toInt())
         } else {
             peekInBlock(blockIndex + 1, offset - remainingInCurrentBuffer)
         }
     }
-
+    
     /**
      * A buffer that can be loaded with data from [inputChannel] asynchronously
      */
-    private inner class LoadableBuffer() {
+    private inner class LoadableBuffer {
         @Volatile
         var ready: Boolean = false
             private set
-
+        
         @Volatile
         var failed: Boolean = false
             private set
-
+        
         /**
          * Error if anything gone wrong (null if [failed] is false). This exception is not thrown, because it happens
          * asynchronously to [nextChar] and we cannot return the read buffer as a result to the asynchronous
@@ -142,35 +139,35 @@ class RingBuffer(private val inputChannel: ScatteringByteChannel) {
          */
         @Volatile
         var failure: IOException? = null
-
+        
         /**
          * Fixed size [ByteBuffer] holding data of the [inputChannel]
          */
         val byteBuffer: ByteBuffer = ByteBuffer.allocateDirect(BUFFER_SIZE).order(ByteOrder.LITTLE_ENDIAN)
-
+        
         fun markEmpty() {
             this.ready = false
         }
-
+        
         fun markReady() {
             // increase the total amount of blocks to be processed
             totalLoadedBlocks += 1
-
+            
             this.ready = true
         }
-
+        
         fun setFailed(exception: IOException) {
             this.failed = true
             this.failure = exception
             setEOF()
             markReady()
         }
-
+        
         fun setEOF() {
             endOfInput = true
             inputChannel.close()
         }
-
+        
         /**
          * Dispatch a loading cycle for this buffer
          */
@@ -179,12 +176,12 @@ class RingBuffer(private val inputChannel: ScatteringByteChannel) {
                 // nothing to do
                 return
             }
-    
+            
             markEmpty()
             withContext(Dispatchers.IO) {
                 // fill exhausted buffer with new data
                 byteBuffer.clear()
-
+    
                 val readBytes = try {
                     inputChannel.read(byteBuffer)
                 } catch (e: ClosedChannelException) {
@@ -193,9 +190,9 @@ class RingBuffer(private val inputChannel: ScatteringByteChannel) {
                     setFailed(e)
                     return@withContext
                 }
-
+    
                 byteBuffer.flip()
-
+    
                 // if we reached EOF, mark that no more dispatches should happen
                 if (readBytes == -1) {
                     setEOF()
@@ -205,7 +202,7 @@ class RingBuffer(private val inputChannel: ScatteringByteChannel) {
                 }
             }
         }
-
+        
         /**
          * Wait until the buffer is ready.
          */
