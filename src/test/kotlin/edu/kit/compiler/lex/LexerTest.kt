d3aeba0da3e616c360fac35ce8fcc4089ad9b7a9
csicar <carsten.csiky@gmail.com>
2021-10-27 16:53:16 +0200

    tests

---
+++ src/test/kotlin/edu/kit/compiler/lex/LexerTest.kt
@@ -0,0 +1,13 @@
+package edu.kit.compiler.lex
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class LexerTest {
+    
+    @Test
+    fun tokenStream() {
+        assertEquals(1, 2)
+    }
+}
