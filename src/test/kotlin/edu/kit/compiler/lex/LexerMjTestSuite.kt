ec3d97280e14d5f2b15d35177ed6148a93c3593e
csicar <carsten.csiky@gmail.com>
2021-10-29 20:24:00 +0200

    Use file path relative to class for tests

--- src/test/kotlin/edu/kit/compiler/lex/LexerMjTestSuite.kt
+++ src/test/kotlin/edu/kit/compiler/lex/LexerMjTestSuite.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.toCollection
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
+import java.io.File
 import java.io.FileInputStream
 import java.nio.file.Path
 import java.nio.file.Paths
@@ -20,6 +21,12 @@ import kotlin.test.assertTrue
 
 internal class LexerMjTestSuite {
     
+    class TestFileArgument(val name: String, val path: Path) {
+        // This is used for naming in the junit output
+        override fun toString(): String = name
+        
+    }
+    
     companion object {
         /** this is used to run multiple instances of the test:
          * see: https://blog.oio.de/2018/11/13/how-to-use-junit-5-methodsource-parameterized-tests-with-kotlin/
@@ -28,42 +35,45 @@ internal class LexerMjTestSuite {
          *         shouldn't be verbose
          */
         @JvmStatic
-        fun provideValidTests(): Stream<Path> {
-            val path = Paths.get("test-cases", "lexer")
-            return path.listDirectoryEntries("*.mj").map { path.relativize(it).also { println(it) } }.stream()
+        fun provideValidTests(): Stream<TestFileArgument> {
+            // https://stackoverflow.com/questions/320542/how-to-get-the-path-of-a-running-jar-file
+            val testFolderAbsolutePath =
+                    File(LexerMjTestSuite::class.java.protectionDomain.codeSource.location.toURI()).getPath()
+            val projectRootDirectory = Paths.get(testFolderAbsolutePath).parent.parent.parent.parent
+            val path = projectRootDirectory.resolve("test-cases").resolve("lexer")
+            return path.listDirectoryEntries("*.mj").map { TestFileArgument(path.relativize(it).name, it) }.stream()
         }
     
     }
     
     @ParameterizedTest
     @MethodSource("provideValidTests")
-    fun test_lexer(path: Path) {
-        val absolutePath = Paths.get("test-cases", "lexer").toAbsolutePath()
-        val inputFile = absolutePath.resolve(path)
-        val outputFile = absolutePath.resolve(path.name + ".out")
-    
+    fun test_lexer(testConfig: TestFileArgument) {
+        val inputFile = testConfig.path
+        val outputFile = testConfig.path.parent.resolve(testConfig.name + ".out")
+        
         println("For input $inputFile expect $outputFile")
-    
+        
         val input = RingBuffer(FileInputStream(inputFile.toFile()).channel)
-    
-    
+        
+        
         val stringTable = StringTable().apply {
             initializeKeywords()
         }
         val lexer = Lexer(input, stringTable)
-    
+        
         val tokens: List<Token> = runBlocking {
             lexer.tokenStream().toCollection(mutableListOf())
         }
-    
-        if (path.name.endsWith("invalid.mj")) {
+        
+        if (testConfig.name.endsWith("invalid.mj")) {
             assertTrue("Expected an invalid token") { tokens.any { it is Token.ErrorToken } }
         } else {
             val expected = outputFile.readLines()
-    
+            
             assertEquals(expected, tokens.lexTestRepr)
         }
-    
-    
+        
+        
     }
 }
