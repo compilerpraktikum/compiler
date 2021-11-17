29416769d6eb2e7eb59efceea0a97aa89d543426
Johannes Hengstler <ubezl@student.kit.edu>
2021-11-17 01:23:19 +0100

    exemplary recovery tests

--- src/test/kotlin/edu/kit/compiler/parser/RecoveryTest.kt
+++ src/test/kotlin/edu/kit/compiler/parser/RecoveryTest.kt
@@ -2,33 +2,60 @@ package edu.kit.compiler.parser
 
 import edu.kit.compiler.error.AnnotationFormatter
 import edu.kit.compiler.lex.SourceFile
-import edu.kit.compiler.lex.SourcePosition
 import edu.kit.compiler.utils.createLexer
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 /**
- * Test cases for correct error recovery. Probably not exhaustive
+ * Test cases for correct error recovery. Probably not exhaustive. Test cases are strings were the parser-defined error
+ * position is marked with '#'. The test routine will determine the positions of '#' characters, remove those characters
+ * and then assert that the errors are found there.
  */
+
 class RecoveryTest {
+
     @Test
     fun testParameterParsing() {
-        testParse("class a { public void foo(int a int a) {} }")
+        testParse("class a { public void foo(#final int a, a#, a#) { } }")
+    }
+
+    @Test
+    fun testExpressionParsing() {
+        testParse("class a { public void foo() { a + b + #+ c; #--d; int c = d[#]#d; (a = b) = c; } }")
+    }
+
+    @Test
+    fun testClasses() {
+        testParse("class #class class class class##")
     }
 
     @OptIn(ExperimentalStdlibApi::class)
-    private fun testParse(input: String, vararg expectedErrorPositions: SourcePosition) {
-        val (lexer, sourceFile) = createLexer(input)
+    private fun testParse(input: String) {
+        // determine error positions
+        val errorPositions = mutableListOf<Int>()
+        var curr = -1
+        do {
+            curr = input.indexOf('#', curr + 1)
+
+            if (curr > -1)
+                errorPositions.add(curr - errorPositions.size)
+        } while (curr > -1)
+
+        val (lexer, sourceFile) = createLexer(input.replace("#", ""))
         val parser = Parser(sourceFile, lexer.tokens())
         parser.parse()
 
         sourceFile.printAnnotations(AnnotationFormatter.DEFAULT)
 
         val annotations = sourceFile.getAnnotations().toList()
-        assertEquals(expectedErrorPositions.size, annotations.size)
+        assertEquals(
+            errorPositions.size,
+            annotations.size,
+            "found ${if (errorPositions.size > annotations.size) "less" else "more"} errors than expected"
+        )
 
         for ((index, a) in annotations.map(SourceFile.Annotation::range).withIndex()) {
-            // todo check that expected source positions of annotations match results
+            assertEquals(errorPositions[index], a.start.offset)
         }
     }
 }
