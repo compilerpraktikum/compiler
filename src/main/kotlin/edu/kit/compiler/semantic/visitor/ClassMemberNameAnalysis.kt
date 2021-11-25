319ad39316cbc8c227a0c5ff3402c19fb0ada971
Tobias Stickling <uudyh@student.kit.edu>
2021-11-25 13:19:50 +0100

    remove unneeded old files

--- src/main/kotlin/edu/kit/compiler/semantic/visitor/ClassMemberNameAnalysis.kt
+++
@@ -1,8 +0,0 @@
-package edu.kit.compiler.semantic.visitor
-
-import edu.kit.compiler.lex.AnnotationType
-import edu.kit.compiler.lex.SourceFile
-import edu.kit.compiler.lex.SourceNote
-import edu.kit.compiler.semantic.AstNode
-import edu.kit.compiler.semantic.Namespace
-
