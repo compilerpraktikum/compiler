a264ab73ef3671fce2e8236fd3ab20c798597571
Robin Schulz <uyduk@student.kit.edu>
2021-11-26 19:25:11 +0100

    remove obsolete util methods

--- src/main/kotlin/edu/kit/compiler/semantic/visitor/Util.kt
+++ src/main/kotlin/edu/kit/compiler/semantic/visitor/Util.kt
@@ -4,9 +4,6 @@ package edu.kit.compiler.semantic.visitor
 import edu.kit.compiler.lex.AnnotationType
 import edu.kit.compiler.lex.SourceFile
 import edu.kit.compiler.lex.SourceRange
-import edu.kit.compiler.semantic.AstNode
-import edu.kit.compiler.semantic.ParsedType
-import edu.kit.compiler.semantic.SemanticType
 
 /**
  * Function name alias for [run], to make code more expressive. We can call [kotlin.collections.MutableMap.putIfAbsent]
@@ -14,24 +11,6 @@ import edu.kit.compiler.semantic.SemanticType
  * return `null`, if no such symbol exists yet).
  */
 fun <T> T.onError(block: T.() -> Unit) = run(block)
-/**
- * Construct a [SemanticType] from a [ParsedType] and a [AstNode.ClassDeclaration] if required.
- */
-fun constructSemanticType(parsedType: ParsedType, typeDeclaration: AstNode.ClassDeclaration? = null): SemanticType {
-    return when (parsedType) {
-        ParsedType.BoolType -> SemanticType.BoolType
-        ParsedType.IntType -> SemanticType.IntType
-        ParsedType.VoidType -> SemanticType.VoidType
-        is ParsedType.ArrayType -> SemanticType.ArrayType(
-            constructSemanticType(
-                parsedType.elementType,
-                typeDeclaration
-            )
-        )
-        is ParsedType.ComplexType -> SemanticType.ComplexType(typeDeclaration!!.name.symbol, typeDeclaration)
-    }
-}
-
 /**
  * Annotate a given source file with an error if the given function evaluates to false.
  */
@@ -44,26 +23,3 @@ fun checkAndAnnotateSourceFileIfNot(sourceFile: SourceFile, sourceRange: SourceR
         )
     }
 }
-
-/**
- * Recursively retrieves the base type of an array
- */
-val ParsedType.baseType: ParsedType
-    get() = when (this) {
-        is ParsedType.ArrayType -> this.elementType.baseType
-        else -> this
-    }
-
-/**
- * Determines an array's dimension
- */
-val ParsedType.dimension: Int
-    get() {
-        var dim = 0
-        var type = this
-        while (type is ParsedType.ArrayType) {
-            dim += 1
-            type = type.elementType
-        }
-        return dim
-    }
