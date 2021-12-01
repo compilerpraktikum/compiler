b69958527a5d930006d148ebdb881b772b02ae77
Johannes Hengstler <ubezl@student.kit.edu>
2021-12-01 15:58:26 +0100

    fixed code sample

--- src/main/kotlin/edu/kit/compiler/transform/CodeSamples.kt
+++ src/main/kotlin/edu/kit/compiler/transform/CodeSamples.kt
@@ -6,10 +6,7 @@ import edu.kit.compiler.semantic.visitor.AbstractVisitor
 
 class ConstructExpressions : AbstractVisitor() {
     override fun visitBinaryOperation(binaryOperation: AstNode.Expression.BinaryOperation) {
-        FirmContext.binaryExpression(binaryOperation.operation) {
-            // call the visitor for all inner expressions here, so the constructed inner expressions are used as
-            // parameters for the outer expression
-            super.visitBinaryOperation(binaryOperation)
-        }
+        super.visitBinaryOperation(binaryOperation)
+        FirmContext.binaryExpression(binaryOperation.operation)
     }
 }
