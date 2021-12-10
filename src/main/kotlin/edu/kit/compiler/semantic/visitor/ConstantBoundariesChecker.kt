77f9d281cbe3e66480afae4c5256cfb351205489
Johannes Hengstler <ubezl@student.kit.edu>
2021-12-10 18:10:37 +0100

    fixed bug in boundary checks: unary minus no longer falsely propagated in through visitor

--- src/main/kotlin/edu/kit/compiler/semantic/visitor/ConstantBoundariesChecker.kt
+++ src/main/kotlin/edu/kit/compiler/semantic/visitor/ConstantBoundariesChecker.kt
@@ -10,17 +10,37 @@ import java.math.BigInteger
  */
 class ConstantBoundariesChecker(val sourceFile: SourceFile) : AbstractVisitor() {
 
-    private var parentIsUnaryMinus: Boolean = false
-
     override fun visitUnaryOperation(unaryOperation: AstNode.Expression.UnaryOperation) {
         if (unaryOperation.operation == AST.UnaryExpression.Operation.MINUS) {
-            parentIsUnaryMinus = true
+            when (unaryOperation.inner) {
+                is AstNode.Expression.LiteralExpression.LiteralIntExpression -> {
+                    this.specialVisitLiteralIntExpression(unaryOperation.inner, true)
+                    // do not descent to avoid double-visit
+                }
+                else -> super.visitUnaryOperation(unaryOperation)
+            }
+        } else {
+            super.visitUnaryOperation(unaryOperation)
         }
-        super.visitUnaryOperation(unaryOperation)
-        parentIsUnaryMinus = false
     }
 
     override fun visitLiteralIntExpression(literalIntExpression: AstNode.Expression.LiteralExpression.LiteralIntExpression) {
+        // if this method is visited, it means that the parent cannot be a unary minus expression,
+        // since the visitor does not descent into an integer literal in that case
+        this.specialVisitLiteralIntExpression(literalIntExpression, false)
+    }
+
+    /**
+     * A special visitor that gets a flag whether the parent expression is a unary minus as additional context.
+     *
+     * @param literalIntExpression a [AstNode.Expression.LiteralExpression.LiteralIntExpression]
+     * @param parentIsUnaryMinus true, iff [literalIntExpression] is directly (i.e. not transitively) embedded in a
+     * [AstNode.Expression.UnaryOperation] with [AST.UnaryExpression.Operation.MINUS] as its type.
+     */
+    private fun specialVisitLiteralIntExpression(
+        literalIntExpression: AstNode.Expression.LiteralExpression.LiteralIntExpression,
+        parentIsUnaryMinus: Boolean
+    ) {
         fun AstNode.Expression.LiteralExpression.LiteralIntExpression.isValidLiteral(): Boolean {
             return if (parentIsUnaryMinus && !isParentized) {
                 BigInteger(value) <= BigInteger("2147483648")
@@ -37,7 +57,5 @@ class ConstantBoundariesChecker(val sourceFile: SourceFile) : AbstractVisitor() 
                 "integer literal value is out of range" at literalIntExpression.sourceRange
             }
         }
-        literalIntExpression.value
-        super.visitLiteralIntExpression(literalIntExpression)
     }
 }
