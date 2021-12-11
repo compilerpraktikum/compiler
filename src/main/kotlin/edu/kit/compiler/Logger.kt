3e63f2c4a5cb97294807cc26c39cac711f56d3c5
Tobias Stickling <uudyh@student.kit.edu>
2021-12-11 05:27:07 +0100

    Add very basic logger

---
+++ src/main/kotlin/edu/kit/compiler/Logger.kt
@@ -0,0 +1,33 @@
+package edu.kit.compiler
+
+object Logger {
+    enum class Level(val prefix: String, val severity: Int) {
+        OFF("", 0),
+        FATAL("FATAL", 10),
+        ERROR("ERROR", 20),
+        WARNING("WARNING", 30),
+        INFO("INFO", 40),
+        DEBUG("DEBUG", 50),
+        TRACE("TRACE", 60),
+        ALL("", Int.MAX_VALUE)
+    }
+
+    var level: Level = Level.INFO
+
+    private fun isEnabledFor(level: Level): Boolean {
+        return level.severity <= this.level.severity
+    }
+
+    fun log(level: Level, message: () -> String) {
+        if (isEnabledFor(level)) {
+            println("[${level.prefix}] ${message()}")
+        }
+    }
+
+    fun fatal(message: () -> String) = log(Level.FATAL, message)
+    fun error(message: () -> String) = log(Level.ERROR, message)
+    fun warning(message: () -> String) = log(Level.WARNING, message)
+    fun info(message: () -> String) = log(Level.INFO, message)
+    fun debug(message: () -> String) = log(Level.DEBUG, message)
+    fun trace(message: () -> String) = log(Level.TRACE, message)
+}
