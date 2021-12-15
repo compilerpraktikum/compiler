8d7d3f714f60bf60936a9e62e635440736ad92ed
Tobias Stickling <uudyh@student.kit.edu>
2021-12-15 17:03:16 +0100

    Add documentation to CliUtils

--- src/main/kotlin/edu/kit/compiler/CliUtils.kt
+++ src/main/kotlin/edu/kit/compiler/CliUtils.kt
@@ -8,8 +8,6 @@ import com.github.ajalt.clikt.parameters.options.OptionWithValues
 import com.github.ajalt.clikt.parameters.options.RawOption
 import com.github.ajalt.clikt.parameters.options.ValueTransformer
 
-// TODO: documentation
-
 private fun mvar(choices: Iterable<String>): String {
     return choices.joinToString("|", prefix = "[", postfix = "]")
 }
@@ -17,6 +15,19 @@ private fun errorMessage(context: Context, choice: String, choices: Map<String, 
     return context.localization.invalidChoice(choice, choices.keys.toList())
 }
 
+/**
+ * Convert the option based on a fixed set of values. Allows to specify multiple values separated by [separator].
+ *
+ * **Example:**
+ * ```
+ * option().choices(mapOf("foo" to 1, "bar" to 2))
+ * ```
+ *
+ * @param[separator] separator if multiple values are given (default: `,`)
+ * @param[ignoreCase] if `true`, the option will accept values case-insensitive (default: `false`)
+ *
+ * @return a list of all chosen options
+ */
 fun <T : Any> RawOption.choices(
     choices: Map<String, T>,
     metavar: String = mvar(choices.keys),
@@ -58,11 +69,16 @@ private fun <T : Any> List<T>.getDuplicates(): Set<T> {
     return duplicates
 }
 
-fun <T : Any> OptionWithValues<List<T>, List<T>, T>.distinct(): OptionWithValues<Set<T>, Set<T>, T> {
+/**
+ * Converts an option that produces a list of values into a set of values.
+ *
+ * @param[allowDuplicates] if `false`, the option is rejected if any duplicates are found (default: `false`)
+ */
+fun <T : Any> OptionWithValues<List<T>, List<T>, T>.distinct(allowDuplicates: Boolean = false): OptionWithValues<Set<T>, Set<T>, T> {
     val distinctTransformer: ArgsTransformer<T, Set<T>> = {
         val values = transformEach(it)
         val set = values.toSet()
-        if (values.size != set.size) {
+        if (!allowDuplicates && values.size != set.size) {
             val duplicates = values.getDuplicates()
             throw CliktError("Error: Duplicated ${if (duplicates.size > 1) "entries" else "entry"} for \"$name\": " + duplicates.joinToString(", "))
         }
