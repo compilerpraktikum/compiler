c8686e8108fff07deb038ceb48299d5def975d0e
Tobias Stickling <uudyh@student.kit.edu>
2022-01-21 21:46:17 +0100

    Implement general optimization infrastructure + refactor constant folding

--- src/main/kotlin/edu/kit/compiler/optimization/Optimization.kt
+++ src/main/kotlin/edu/kit/compiler/optimization/Optimization.kt
@@ -1,17 +1,36 @@
 package edu.kit.compiler.optimization
 
 import edu.kit.compiler.Compiler
+import edu.kit.compiler.Logger
+import firm.Graph
 import firm.Program
 
 /**
- * Perform constant propagation and folding.
+ * Optimizes the given [Graph]. Returns whether any changes have been made.
  */
-private fun constantPropagationAndFolding() {
-    Program.getGraphs().forEach {
-        doConstantPropagationAndFolding(it)
-    }
+typealias Optimization = (Graph) -> Boolean
+
+private fun getOptimizationsForLevel(level: Compiler.OptimizationLevel): List<Optimization> = when (level) {
+    Compiler.OptimizationLevel.Base -> listOf(::doConstantPropagationAndFolding)
+    Compiler.OptimizationLevel.Full -> listOf(
+        ::doConstantPropagationAndFolding,
+    )
+}
+
+private fun runOptimizations(graph: Graph, optimizations: List<Optimization>) {
+    do {
+        var hasChangedAtLeastOnce = false
+        optimizations.forEach { opt ->
+            val hasChanged = opt(graph)
+            hasChangedAtLeastOnce = hasChangedAtLeastOnce || hasChanged
+        }
+    } while (hasChangedAtLeastOnce)
 }
 
 fun doOptimization(level: Compiler.OptimizationLevel) {
-    constantPropagationAndFolding()
+    Logger.debug { "Running optimizations (level: ${level.intValue}) ..." }
+    val optimizations = getOptimizationsForLevel(level)
+    Program.getGraphs().forEach {
+        runOptimizations(it, optimizations)
+    }
 }
