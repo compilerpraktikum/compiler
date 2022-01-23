55405c08ce89c61e55a92d9eb69d05d0079b8c72
Tobias Stickling <uudyh@student.kit.edu>
2022-01-23 23:50:52 +0100

    Use debug version of firm

--- src/main/kotlin/edu/kit/compiler/transform/FirmContext.kt
+++ src/main/kotlin/edu/kit/compiler/transform/FirmContext.kt
@@ -77,7 +77,7 @@ object FirmContext {
      * Initialize the firm framework
      */
     fun init() {
-        Firm.VERSION = Firm.FirmVersion.RELEASE
+        Firm.VERSION = Firm.FirmVersion.DEBUG
         Firm.init("x86_64-linux-gnu", arrayOf("pic=1"))
         Logger.debug { "Initialized libFirm Version: ${Firm.getMajorVersion()}.${Firm.getMinorVersion()}" }
 
