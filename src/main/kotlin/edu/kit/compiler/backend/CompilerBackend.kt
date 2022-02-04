8f64441bfd463b9ebc0fbe074e00cf1d3cfe69ce
Tobias Stickling <uudyh@student.kit.edu>
2022-02-04 01:06:52 +0100

    fix fpe

--- src/main/kotlin/edu/kit/compiler/backend/CompilerBackend.kt
+++ src/main/kotlin/edu/kit/compiler/backend/CompilerBackend.kt
@@ -13,6 +13,7 @@ class CompilerBackend(
     private val dumpMolki: Boolean = false,
 ) : Backend {
     override fun generate() {
+        fixJavaCompatibility()
         val graphs = Program.getGraphs()
         val codeGenFacade = CodeGenFacade(graphs, dumpCodeGenIR = dumpCodeGenIR, dumpMolkIR = dumpMolki)
         val functions = codeGenFacade.generate()
